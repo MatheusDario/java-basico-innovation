701cd870b6e511189e2c0f21ab30d1c1540aef1c
Matheus Dario <dario.matheus97@gmail.com>
2021-07-27 14:42:13 -0300

    encapsulamento-java

--- teste/test2/src/main/java/Pessoa.java
+++ teste/test2/src/main/java/Pessoa.java
@@ -1,14 +1,30 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Pessoa {
-    private String nome = "Matheus";
-
-    public String getNome() {
-        return nome;
+    public Pessoa(String nome, int dia, int mes, int ano) { //construtor que recebe as informacoes
+        this.nome = nome;
+        this.dataNascimento = LocalDate.of(ano, mes, dia);
     }
+    private String nome;
+    private LocalDate dataNascimento;
 
-    public String falarMeuNome() {
-        return "Ola meu nome e " + getNome();
+    public String getNome() {return nome;}
+
+   private void setNome(String nome) { //e possivel alterar o nome
+        this.nome = nome;
+   }
+
+    public LocalDate getDataNascimento() {return dataNascimento;}
+
+    public int calculaIdade() {return Period.between(dataNascimento, LocalDate.now()).getYears();}
+
+    public static void main(String[] args) {
+        Pessoa eu = new Pessoa("Matheus", 15,9,1997);
+        eu.setNome("Matheus Dario");
+        System.out.println(eu.getNome());
+        System.out.println(eu.getDataNascimento());
+        System.out.println(eu.calculaIdade());
     }
 }
-
-
 
--- teste/test2/src/main/java/PessoaNome.java
+++
@@ -1,8 +0,0 @@
-public class PessoaNome {
-    public static void main(String[] args) {
-
-        Pessoa pessoa = new Pessoa();
-
-        System.out.println(pessoa.falarMeuNome());
-    }
-}
