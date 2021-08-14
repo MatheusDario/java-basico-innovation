ccbbac2f5e1f0c229016a7401e374935d1334457
Matheus Dario <dario.matheus97@gmail.com>
2021-08-14 15:53:12 -0300

    list-arraylist-java

---
+++ collections-course/src/main/java/one/digitalinnovation/list/ExemploVector.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation.list;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ExemploVector {
+    public static void main(String[] args) {
+
+        List<String> esportes = new Vector<>();
+
+        esportes.add("Futebol");
+        esportes.add("Basquete");
+        esportes.add("Tenis de mesa");
+        esportes.add("Handboll");
+
+        esportes.set(2, "ping pong");
+        System.out.println(esportes);
+
+        esportes.remove(2);
+        System.out.println(esportes);
+
+        esportes.remove("Basquete");
+
+        System.out.println(esportes.get(0));
+
+        for(String esporte: esportes) {
+            System.out.println(esporte);
+        }
+    }
+}
